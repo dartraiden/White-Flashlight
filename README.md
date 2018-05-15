69ed8159b90f99c7ca0401946c8a2b747d6f0c84
dartraiden <wowemuh@gmail.com>
2018-05-15 20:10:48 +0300

    Add firs working version of code

---
+++ README.md
@@ -0,0 +1,11 @@
+# **White Flashlight**
+
+## Description
+This Magisk module enables the white flashlight on Xiaomi Redmi Note 4 Snapdragon. Supports custom ROMs, as well as Treble-based ROMs.
+
+## Requirements
+- Xiaomi Redmi Note 4 Snapdragon
+- Magisk v15+
+
+## Links
+[Latest stable Magisk](http://www.tiny.cc/latestmagisk)
---
+++ common/post-fs-data.sh
@@ -0,0 +1,10 @@
+#!/system/bin/sh
+# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
+# This will make your scripts compatible even if Magisk change its mount point in the future
+MODDIR=${0%/*}
+
+# This script will be executed in post-fs-data mode
+# Enable Hi_performance mode of your wcd9xx DAC
+su
+chmod 0777 $MODDIR/post-fs-data.sh
+busybox echo "0" > /sys/devices/soc/qpnp-flash-led-25/leds/led:torch_1/max_brightness
